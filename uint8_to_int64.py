0b1c7e2e4f7a59a5ae4781f61abf028c614114b3
yuzhen chen <yuzhench@umich.edu>
2023-08-13 10:40:48 -0400

    Add the unit8_to_int64 file to the git

---
+++ uint8_to_int64.py
@@ -0,0 +1,11 @@
+import numpy as np
+import glob
+
+input_file_direction = "/home/yuzhen/Desktop/first/Azure-Kinect-Samples/opencv-kinfu-samples/build/red"
+
+file_list = glob.glob(input_file_direction+'/*_semantic.npy')
+
+for file in file_list:
+    arr_int64 = np.load(file)
+    arr_uint8 = arr_int64.astype(np.uint8)
+    np.save(file,arr_uint8)
